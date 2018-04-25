6504790fa4299ced69d3b42a65e2cab7463d6054
jason <182215513386@163.com>
2018-04-25 17:37:53 +0800

    首页优化

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -10,7 +10,7 @@ class IndexPage extends Component{
   render(){
     return(
       <div className={styles.normal}>
-        <h1 className={styles.title}>Yay! Welcome to</h1>
+        <h1 className={styles.title}>123</h1>
         <div className={styles.welcome} />
         <ul className={styles.list}>
           <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
