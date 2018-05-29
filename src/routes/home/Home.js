8db88bf8bc7a4ad2141c305fce703a81e04eed5d
jason <182215513386@163.com>
2018-05-29 10:11:51 +0800

    左侧菜单栏实现

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -50,7 +50,7 @@ class Home extends Component {
   toggle = () => this.setState({collapsed: !this.state.collapsed});
   render() {
     const {collapsed} = this.state;
-    const {userInfoData} = this.props.home;
+    const {userInfoData,menuData} = this.props.home;
     return (
       <Layout className={styles.layoutContent}>
         <Header className={styles.header}>
@@ -77,34 +77,20 @@ class Home extends Component {
         <Layout>
           <Sider width={200} collapsed={collapsed}>
             <div className="logo"/>
-            <Menu defaultSelectedKeys={['1']} mode="inline" className={styles.layoutMenu}>
-              <Menu.Item key="1">
-                <Icon type="pie-chart"/>
-                <span>Option 1</span>
-              </Menu.Item>
-              <Menu.Item key="2">
-                <Icon type="desktop"/>
-                <span>Option 2</span>
-              </Menu.Item>
-              <SubMenu
-                key="sub1"
-                title={<span><Icon type="user"/><span>User</span></span>}
-              >
-                <Menu.Item key="3">Tom</Menu.Item>
-                <Menu.Item key="4">Bill</Menu.Item>
-                <Menu.Item key="5">Alex</Menu.Item>
-              </SubMenu>
-              <SubMenu
-                key="sub2"
-                title={<span><Icon type="team"/><span>Team</span></span>}
-              >
-                <Menu.Item key="6">Team 1</Menu.Item>
-                <Menu.Item key="8">Team 2</Menu.Item>
-              </SubMenu>
-              <Menu.Item key="9">
-                <Icon type="file"/>
-                <span>File</span>
-              </Menu.Item>
+             <Menu  mode="inline" className={styles.layoutMenu}>
+               {(menuData && menuData.length!== 0) && menuData.map((item)=>
+                 !item.SubMenus ?
+                   <Menu.Item key={item.ID}>
+                     <Icon type="pie-chart"/>
+                     <span>{item.Name}</span>
+                   </Menu.Item> :
+                   <SubMenu
+                     key={item.ID}
+                     title={<span><Icon type="user"/><span>{item.Name}</span></span>}
+                   >
+                     {item.SubMenus && item.SubMenus.map((menu) => <Menu.Item key={menu.ID}>{menu.Name}</Menu.Item>)}
+                   </SubMenu>
+               )}
             </Menu>
           </Sider>
           <Layout className={styles.layoutContentTwo}>
